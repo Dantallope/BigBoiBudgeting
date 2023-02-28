01d125bc36e073d7ca9bca53993a931356e47dab
Jerry Wan <craftersol728@gmail.com>
2023-02-27 20:07:06 -0800

    added nav bar and started on the css

---
+++ src/navBar.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './NavBar.css';
+import './css/navBar.css';
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="/path/to/logo.png" alt="Logo" />
+      </div>
+      <ul className="navbar-menu">
+        <li className="navbar-item">
+          <a href="#" className="navbar-link">Tab 1</a>
+          <ul className="navbar-dropdown">
+            <li><a href="#">Option 1</a></li>
+            <li><a href="#">Option 2</a></li>
+            <li><a href="#">Option 3</a></li>
+          </ul>
+        </li>
+        <li className="navbar-item">
+          <a href="#" className="navbar-link">Tab 2</a>
+          <ul className="navbar-dropdown">
+            <li><a href="#">Option 1</a></li>
+            <li><a href="#">Option 2</a></li>
+            <li><a href="#">Option 3</a></li>
+          </ul>
+        </li>
+        <li className="navbar-item">
+          <a href="#" className="navbar-link">Tab 3</a>
+          <ul className="navbar-dropdown">
+            <li><a href="#">Option 1</a></li>
+            <li><a href="#">Option 2</a></li>
+            <li><a href="#">Option 3</a></li>
+          </ul>
+        </li>
+        <li className="navbar-item">
+          <a href="#" className="navbar-link">Tab 4</a>
+          <ul className="navbar-dropdown">
+            <li><a href="#">Option 1</a></li>
+            <li><a href="#">Option 2</a></li>
+            <li><a href="#">Option 3</a></li>
+          </ul>
+        </li>
+        <li className="navbar-item">
+          <a href="#" className="navbar-link">Tab 5</a>
+          <ul className="navbar-dropdown">
+            <li><a href="#">Option 1</a></li>
+            <li><a href="#">Option 2</a></li>
+            <li><a href="#">Option 3</a></li>
+          </ul>
+        </li>
+      </ul>
+      <div className="navbar-auth">
+        <button className="navbar-signin">Sign In</button>
+        <button className="navbar-login">Log In</button>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
