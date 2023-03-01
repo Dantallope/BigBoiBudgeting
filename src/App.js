ad272a9f9bf81bf8408b1cedd9c4bff64bcea0a1
erech <sofiahanapin@gmail.com>
2023-02-28 22:43:30 -0800

    add login style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './login.js';
 import AppNavBar from './navBar.js';
 import AppSpendingOverview from './overview.js'
 
 function App() {
+  const [showLogin, setLogin] = useState(true) ;
+
+  function handleClick() {
+    setLogin(!showLogin);
+  }
+
   return (
     <div>
-      
       <h1>Welcome to BigBoiBudgeting app!</h1>
       <AppNavBar />
       <AppSpendingOverview />
-      <Login />
+
+      <div> {showLogin ? <Login onClick={handleClick} /> : <AppNavBar onClick={handleClick} />} </div>
+
     </div>
   );
 }
