7728c35dc1a58990e044c33e6c3dfba5d6a739b9
erech <sofiahanapin@gmail.com>
2023-02-28 19:27:33 -0800

    add login js and css files

--- src/navBar.js
+++ src/navBar.js
@@ -9,7 +9,7 @@ function NavBar() {
       </div>
       <ul className="navbar-menu">
         <li className="navbar-item">
-          <a href="#" className="navbar-link">Overview</a>
+          <a href="#" className="navbar-link">How it works</a>
           <ul className="navbar-dropdown">
             <li><a href="#">Option 1</a></li>
             <li><a href="#">Option 2</a></li>
@@ -17,7 +17,7 @@ function NavBar() {
           </ul>
         </li>
         <li className="navbar-item">
-          <a href="#" className="navbar-link">Monthly</a>
+          <a href="#" className="navbar-link">About Us</a>
           <ul className="navbar-dropdown">
             <li><a href="#">Option 1</a></li>
             <li><a href="#">Option 2</a></li>
@@ -25,7 +25,7 @@ function NavBar() {
           </ul>
         </li>
         <li className="navbar-item">
-          <a href="#" className="navbar-link">Notifications</a>
+          <a href="#" className="navbar-link">Budget</a>
           <ul className="navbar-dropdown">
             <li><a href="#">Option 1</a></li>
             <li><a href="#">Option 2</a></li>
