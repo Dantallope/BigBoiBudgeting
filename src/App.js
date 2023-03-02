37279fd5ef8930268c3844a15a9b6636e6b8ba67
Jerry Wan <craftersol728@gmail.com>
2023-03-01 20:25:18 -0800

    updates with bugs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import NavBar from './NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import AppSpendingOverview from './overview.js';
-import Home from './pages/index'
+import Home from './pages'
+import About from './pages/about'
+import Services from './pages/services'
 
 
 
@@ -14,6 +16,8 @@ function App() {
       <NavBar />
         <Routes>
           <Route path="/" exact component = { Home } />
+          <Route path="/about" exact component = { About } />
+          <Route path="/service" exact component = { Services } />
         </Routes>
       <AppSpendingOverview />
     </Router>
