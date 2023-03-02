2e0786119466d166c5449d20e5e5fd7e504a9b62
Jerry Wan <craftersol728@gmail.com>
2023-03-01 20:54:27 -0800

    added all routes for navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './css/App.css'
 import NavBar from './NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import AppSpendingOverview from './overview.js';
+// import AppSpendingOverview from './overview.js';
 import Home from './pages'
 import About from './pages/about'
 import Services from './pages/services'
+import ContactUs from './pages/contact-us'
+import SignIn from './pages/sign-in'
+import SignUp from './pages/sign-up'
 
 
 
@@ -18,8 +21,10 @@ function App() {
           <Route path="/" exact element={ <Home/> } />
           <Route path="/about"  element={ <About/> } />
           <Route path="/services"  element={ <Services/> } />
+          <Route path="/contact-us"  element={ <ContactUs/> } />
+          <Route path="/sign-in"  element={ <SignIn/> } />
+          <Route path="/sign-up"  element={ <SignUp/> } />
         </Routes>
-      <AppSpendingOverview />
     </Router>
   );
 }
---
+++ src/pages/sign-in.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div style = {{display:'flex',justifyContent:'center', alignContent:'center',height:'90vh'}}>
+        <h1>Sign In</h1>
+    </div>
+  )
+}
+
+export default SignIn
